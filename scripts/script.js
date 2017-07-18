9ca07f5fee3345080614beb3b0a437477ac01b72
aleezay <aleezayh@hotmail.com>
2017-07-18 01:35:59 -0700

    fixed header, updated newsletter section, but lost footer

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,16 @@ $('.main-carousel').flickity({
   textAlign: 'center',
   contain: true
 });
+
+// $(function(){
+//   $('#subscribe').on("submit", function(event){
+//       event.preventDefault();
+
+//       if ( $('#email'). val().length !== 0 ) {
+//             alert("Thanks for subscribing");
+//       } else {
+//             alert("Please enter a valid email address");
+//       }
+
+//     });
+// });
