68fdc0e83c475c391d7b2ca7711fa2281174f42e
aleezay <aleezayh@hotmail.com>
2017-07-26 16:42:12 -0700

    trying to add accessibility

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+$(function() {
+   $("a[href^='#']").not("a[href='#']").click(function() {
+      $("#"+$(this).attr("href").slice(1)+"").focus();
+   });
+});
 
 $('.carousel').flickity({
   // options
